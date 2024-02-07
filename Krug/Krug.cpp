cf9ed4bc14cc6adf8075b21be0bb9d8d4b5af25d
Kwache01 <amin0625@bk.ru>
2024-02-07 22:00:43 +0300

    Добавьте файлы проекта.

---
+++ Krug/Krug.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculateRingArea(double innerRadius, double outerRadius) {
+    const double PI = std::acos(-1.0);
+    double innerArea = PI * innerRadius * innerRadius;
+    double outerArea = PI * outerRadius * outerRadius;
+    double ringArea = outerArea - innerArea;
+    return ringArea;
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double innerRadius, outerRadius;
+    cout << "Введите внутренний радиус кольца: ";
+    cin >> innerRadius;
+    cout << "Введите внешний радиус кольца: ";
+    cin >> outerRadius;
+
+    double ringArea = calculateRingArea(innerRadius, outerRadius);
+    cout << "Площадь кольца: " << ringArea << endl;
+
+    return 0;
+}
